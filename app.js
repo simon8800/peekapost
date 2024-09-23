a982f6928383742925b8c1ecc5ad9248dd4ad638
Simon Mei <smei8800@gmail.com>
2024-09-23 18:23:48 -0400

    First commit
    UI draft of message board complete
    
    Begin express app skeleton

---
+++ app.js
@@ -0,0 +1,10 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+
+const app = express();
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
